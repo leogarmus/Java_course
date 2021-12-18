849cf468c6896362be95daeea848e498e95ddda0
leogasmus <leogasmus@gmail.com>
2021-12-18 17:01:40 +0200

    add more command

--- src/com/pb/aleksieiev/hw11/Main.java
+++ src/com/pb/aleksieiev/hw11/Main.java
@@ -29,14 +29,14 @@ public class Main {
         waitCommand(book);
 
 
-
     }
 
-    static void waitCommand(List<ContactItem> book){
+    static void waitCommand(List<ContactItem> book) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Список комманд: ");
-        System.out.println("add - для добавления команды, rm - для удалени номера, ls - для показа всех контактов");
+        System.out.println("add - для добавления команды, rm - для удалени номера, ls - для показа всех контактов, fn - поиска" +
+                "\n" + "update - для обновления");
         System.out.println("Введите команду");
         String userCommand = scan.nextLine();
 
@@ -46,16 +46,23 @@ public class Main {
         } else if (userCommand.equals("ls")) {
             showBook(book);
             waitCommand(book);
-
         } else if (userCommand.equals("rm")) {
-
+            removeByName(book);
+            waitCommand(book);
+        } else if (userCommand.equals("fn")) {
+            findByName(book);
+            waitCommand(book);
+        } else if (userCommand.equals("update")) {
+            updateByName(book);
+            waitCommand(book);
         } else {
             System.out.println("Введите доступную команду");
             waitCommand(book);
 
         }
     }
-    static void addContact(List<ContactItem> book){
+
+    static void addContact(List<ContactItem> book) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите ФИО");
         String name = scan.nextLine();
@@ -69,9 +76,70 @@ public class Main {
         book.add(newContact);
         System.out.println("Контакт успешно добавлен");
     }
-    static void showBook(List<ContactItem> book){
+
+    static void showBook(List<ContactItem> book) {
         System.out.println("======Ваши контакты=====");
         System.out.println(book);
         System.out.println("=======================");
+    }
+
+    static int findIndexByName(List<ContactItem> book, String name) {
+        for (int i = 0; i < book.size(); i++) {
+            if (book.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static void removeByName(List<ContactItem> book) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите ФИО");
+        String name = scan.nextLine();
+        for (int i = 0; i < book.size(); i++) {
+            if (book.get(i).getName().equals(name)) {
+                book.remove(i);
+                System.out.println("Контакт удален");
+            } else {
+                System.out.println("Контакт не найден");
+            }
+        }
+    }
+
+    static void findByName(List<ContactItem> book) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите ФИО");
+        String name = scan.nextLine();
+        for (int i = 0; i < book.size(); i++) {
+            if (book.get(i).getName().equals(name)) {
+                System.out.println(book.get(i));
+            } else {
+                System.out.println("Конткакт не найден");
+            }
+        }
+    }
+
+    static void updateByName(List<ContactItem> book) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите ФИО кондакта которое хотите изменить");
+        String name = scan.nextLine();
+        for (int i = 0; i < book.size(); i++) {
+            if (book.get(i).getName().equals(name)) {
+                System.out.println(book.get(i));
+                System.out.println("Введите новое ФИО");
+                String newName = scan.nextLine();
+                System.out.println("Введите номер");
+                String pno = scan.nextLine();
+                System.out.println("Введтите адресс");
+                String address = scan.nextLine();
+                System.out.println("Введите дату рождения в формате гггг-мм-дд");
+                LocalDate dateOfBirth = LocalDate.parse(scan.nextLine());
+                book.get(i).setName(newName);
+                book.get(i).setAddress(address);
+                book.get(i).setPhone(pno);
+                book.get(i).setDateOfBirth(dateOfBirth);
+                book.get(i).setUpdateDate(LocalDate.now());
+            }
+        }
     }
 }
